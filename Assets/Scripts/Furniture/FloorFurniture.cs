cc34ce7f261658e41de4b6316846506644e29ef4
Nav-gtr <nullandvoidproject@gmail.com>
2025-12-09 18:17:52 -0500

    Add files via upload

---
+++ Assets/Scripts/Furniture/FloorFurniture.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorFurniture : Furniture
+{
+
+[SerializeField] private bool fusedToGround;
+[SerializeField] private bool replacesFloor;
+
+}
