999e55ae91bd731093100a771365b8441c4e1170
Nav-gtr <nullandvoidproject@gmail.com>
2025-12-16 12:45:25 -0500

    Update Furniture.cs
    Furniture size is now settable, yay!

--- Assets/Scripts/Furniture/Furniture.cs
+++ Assets/Scripts/Furniture/Furniture.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public abstract class Furniture : Mirror.NetworkBehaviour
 {
 
+[field: SerializeField] public Vector2Int Size { get; protected set; }
+
 [SerializeField][Min(0f)][Tooltip("Sets the cost of the furniture in the house builder.")] private int cost;
 [SerializeField][Tooltip("I have no idea what this does... -NaV")] private bool unique;
 
