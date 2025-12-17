a454d1728f526b30f5c0165038ae9b984d823258
Nav-gtr <nullandvoidproject@gmail.com>
2025-12-17 14:38:06 -0500

    Create DungeonEntrance.cs

---
+++ Assets/Scripts/Land/DungeonEntrance.cs
@@ -0,0 +1,11 @@
+using System;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonEntrance : MonoBehaviour
+{
+	[SerializeField] private Sprite mapSprite;
+	[SerializeField] private Sprite finishedSprite;
+	[SerializeField] private string boss;
+}
